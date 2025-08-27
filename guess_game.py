4f5e61699b4f11a86edc272ce4349811234bf15a
Yatharthaprasad7 <prasad079n@gmail.com>
2025-08-28 01:04:24 +0530

    guess_game.py

--- guess_game.py
+++ guess_game.py
@@ -4,14 +4,17 @@ n=random.randint(0,100) # This show that the guessing game has the limit to gues
 a=-1
 guesses=0 # initialised variable for storing value of number of guesses made by the user
 while(a!=n):
-    guesses+=1 # To update the number of guesses by each new guess
-    a=int(input("Guess a number :- "))
-    if(a>n):
-        print("You are close! Think of a lower value number.") #If the guessed value is more than the actual value so this will pop
-    elif(a<n):
-        print("Woah Great Guess! But Think of a higher value number.") #If the guessed value is less than the actual value so this will pop
-    else:
-        print(f"Congratulations! You have correctly guessed it in {guesses} guesses.")
-
-    
-
+    try:
+        guesses+=1 # To update the number of guesses by each new guess
+        a=int(input("Guess a number :- "))
+        if(a>100):
+            print("Your random number is just between 0 to 100, so kindly guess again.") # So user can play the game more properly by just guessing numbers between 0-100
+        if(a>n):
+            print("Nice Try! Think of a lower value number.") #If the guessed value is more than the actual value so this will pop
+        elif(a<n):
+            print("Woah Great Guess! But Think of a higher value number.") #If the guessed value is less than the actual value so this will pop
+        else:
+            print(f"Congratulations! You have correctly guessed it in {guesses} guesses.")
+    except ValueError:
+        print("Enter a valid number please")
+        continue
